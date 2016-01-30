49026530297d1c98100da30096dfcb7591b84fc5
bernadette <bernadette@bureau.home>
2016-01-30 16:36:29 +0100

    Rename packages

--- src/main/java/fr/herman/memento/MementoApplication.java
+++ src/main/java/fr/herman/memento/MementoApplication.java
@@ -1,4 +1,4 @@
-package fr.herman;
+package fr.herman.memento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/fr/herman/memento/MementoApplicationTests.java
+++ src/test/java/fr/herman/memento/MementoApplicationTests.java
@@ -1,8 +1,11 @@
-package fr.herman;
+package fr.herman.memento;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import fr.herman.memento.MementoApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
