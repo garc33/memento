5536e6cb474af4fdd7e5f756af802a8526e765ce
garc33 <marcherman@192.168.0.48>
2016-02-28 18:03:27 +0100

    reader/writer throw ioexception

--- src/main/java/fr/herman/memento/io/DataReader.java
+++ src/main/java/fr/herman/memento/io/DataReader.java
@@ -1,7 +1,8 @@
 package fr.herman.memento.io;
 
 import java.io.DataInput;
+import java.io.IOException;
 
 public interface DataReader<T> {
-	T read(DataInput input);
+	T read(DataInput input) throws IOException;
 }
--- src/main/java/fr/herman/memento/io/DataWriter.java
+++ src/main/java/fr/herman/memento/io/DataWriter.java
@@ -1,7 +1,8 @@
 package fr.herman.memento.io;
 
 import java.io.DataOutput;
+import java.io.IOException;
 
 public interface DataWriter<T> {
-	long write(T data, DataOutput output);
+	long write(T data, DataOutput output) throws IOException;
 }
